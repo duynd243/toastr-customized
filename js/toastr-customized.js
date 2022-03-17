f5bb55bd3045d6ab7744991f3aa7f5f31a69e0a1
duyndse150396 <duyndse@gmail.com>
2022-03-17 18:58:01 +0700

    init commit

---
+++ js/toastr-customized.js
@@ -0,0 +1,10 @@
+toastr.options.progressBar = true;
+toastr.options.closeButton = true;
+toastr.options.showMethod = 'slideDown';
+
+toastr.options.timeOut = 5000;
+toastr.options.extendedTimeOut = 5000;
+
+// Never hide (just for testing)
+// toastr.options.timeOut = 0;
+// toastr.options.extendedTimeOut = 0;
