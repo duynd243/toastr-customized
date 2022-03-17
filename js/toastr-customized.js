b13e44cc27ccad9404df92d60c80e2626fffffc5
duyndse150396 <duyndse@gmail.com>
2022-03-17 19:21:32 +0700

    customized close icon

--- js/toastr-customized.js
+++ js/toastr-customized.js
@@ -5,6 +5,8 @@ toastr.options.showMethod = 'slideDown';
 toastr.options.timeOut = 5000;
 toastr.options.extendedTimeOut = 5000;
 
+toastr.options.closeHtml = '<div><ion-icon name="close-outline"></ion-icon></div>';
+
 // Never hide (just for testing)
 // toastr.options.timeOut = 0;
 // toastr.options.extendedTimeOut = 0;
